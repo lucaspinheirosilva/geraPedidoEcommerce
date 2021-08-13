fac85f878e6eff4999db27e2e62b1b91c017a6c1
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2021-08-13 18:44:07 -0300

    arrumando detalhes

--- src/main/java/com/vetor/gera_pedido_ecommerce/GeraPedidoEcommerceApplication.java
+++ src/main/java/com/vetor/gera_pedido_ecommerce/GeraPedidoEcommerceApplication.java
@@ -2,10 +2,11 @@ package com.vetor.gera_pedido_ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class GeraPedidoEcommerceApplication {
+public class GeraPedidoEcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeraPedidoEcommerceApplication.class, args);
